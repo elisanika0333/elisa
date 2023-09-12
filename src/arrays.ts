ff30cbde5ac193b7c57003511de679103e70b084
Elisa Nika <elisanika03@Elisas-MacBook-Pro.local>
2023-09-12 18:24:17 -0400

    Function6 array done

--- src/arrays.ts
+++ src/arrays.ts
@@ -68,7 +68,8 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const briefWords = words.filter((word) => word.length < 4);
+    return briefWords.length;
 }
 
 /**

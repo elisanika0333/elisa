b1def26ea99b044917ed697649020bea468f9e27
Elisa Nika <elisanika03@host-0800-c889f3b7a671.pool.net.maine.edu>
2023-10-18 14:49:49 -0400

    Added the missing things

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,27 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    const handleTypeChange = () => {
+        setQuestionType((prevType) =>
+            prevType === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    };
+
+    return (
+        <div>
+            <Button onClick={handleTypeChange}>Change Type</Button>
+            {questionType === "short_answer_question" && (
+                <div>Short Answer</div>
+            )}
+            {questionType === "multiple_choice_question" && (
+                <div>Multiple Choice</div>
+            )}
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-/**
- * Here is a helper function you *must* use to "roll" your die.
- * The function uses the builtin `random` function of the `Math`
- * module (which returns a random decimal between 0 up until 1) in order
- * to produce a random integer between 1 and 6 (inclusive).
- */
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    // Initialize state for the two dice with initial different values
+    const [leftDie, setLeftDie] = useState(d6());
+    const [rightDie, setRightDie] = useState(d6());
+
+    // Function to handle rolling the dice
+    const rollLeftDie = () => {
+        setLeftDie(d6());
+    };
+
+    const rollRightDie = () => {
+        setRightDie(d6());
+    };
+
+    // Check if the dice values are equal (snake eyes) to render the win or lose message
+    const isWin = leftDie === rightDie;
+
+    return (
+        <div>
+            <span data-testid="left-die">{leftDie}</span>
+            <span data-testid="right-die">{rightDie}</span>
+            <Button onClick={rollLeftDie}>Roll Left</Button>
+            <Button onClick={rollRightDie}>Roll Right</Button>
+            {isWin ? <p>You Win!</p> : <p>You Lose!</p>}
+        </div>
+    );
 }
